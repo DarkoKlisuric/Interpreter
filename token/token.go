085bb7958fba83e611820f4e370075a6a809aaf0
Darko Klisurić <klisuric1995@gmail.com>
2020-06-16 20:06:01 +0200

    First part of test

--- token/token.go
+++ token/token.go
@@ -3,28 +3,31 @@ package token
 type TokenType string
 
 type Token struct {
-	Type TokenType
+	Type    TokenType
 	Literal string
 }
 
 const (
 	ILLEGAL = "ILLEGAL" // signifies a token/character we don’t know about
-	EOF = "EOF" // stands for “end of file”
+	EOF     = "EOF"     // stands for “end of file”
 
 	// Identifiers + literals
 	IDENT = "IDENT" // add, foo-bar, x, y, ...
-	INT = "INT" // 1234123
+	INT   = "INT"   // 1234123
 
 	//Operators
 	ASSIGN = "="
-	PLUS = "+"
+	PLUS   = "+"
 
 	LPAREN = "("
 	RPAREN = ")"
 	LBRACE = "{"
 	RBRACE = "}"
 
+	COMMA = ","
+	SEMICOLON = ";"
+
 	// Keywords
 	FUNCTION = "FUNCTION"
-	LET = "LET"
+	LET      = "LET"
 )

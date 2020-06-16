e8443d8a663625bdb6a3dd39b58b6ac224b8d098
Darko Klisurić <klisuric1995@gmail.com>
2020-06-16 19:55:06 +0200

    Creating constant for tokens, keywords etc.

--- token/token.go
+++ token/token.go
@@ -6,3 +6,25 @@ type Token struct {
 	Type TokenType
 	Literal string
 }
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers + literals
+	IDENT = "IDENT" // add, foo-bar, x, y, ...
+	INT = "INT" // 1234123
+
+	//Operators
+	ASSIGN = "="
+	PLUS = "+"
+
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)

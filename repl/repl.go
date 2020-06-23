cf15ac95bcc603ae398715415d269514250a5e01
Darko Klisurić <klisuric1995@gmail.com>
2020-06-23 19:40:50 +0200

    Creating REPL "Read Eval Print Loop"

--- repl/repl.go
+++ repl/repl.go
@@ -1,1 +1,35 @@
 package repl
+
+import (
+	"bufio"
+	"fmt"
+	"github.com/DarkoKlisuric/interpreter/lexer"
+	"github.com/DarkoKlisuric/interpreter/token"
+	"io"
+)
+
+const PROMPT = ">> "
+
+// REPL stands for “Read Eval Print Loop”
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+
+	for {
+		fmt.Println(PROMPT)
+		scanned := scanner.Scan()
+
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+
+		l := lexer.New(line)
+
+		// Looping through inpunts (tokens)
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}

8e9c15e449707f7fe9ec0ff422363f9c3f88939d
Darko Klisurić <klisuric1995@gmail.com>
2020-06-25 19:50:52 +0200

    Added errors in parser struct0

--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,7 @@
 package parser
 
 import (
+	"fmt"
 	"github.com/DarkoKlisuric/interpreter/ast"
 	"github.com/DarkoKlisuric/interpreter/lexer"
 	"github.com/DarkoKlisuric/interpreter/token"
@@ -10,16 +11,30 @@ type Parser struct {
 	l         *lexer.Lexer
 	curToken  token.Token
 	peekToken token.Token
+	errors    []string
 }
 
 func New(l *lexer.Lexer) *Parser {
-	p := &Parser{l: l}
+	p := &Parser{
+		l:      l,
+		errors: []string{},
+	}
 
 	// Read two tokens, so curToken and peekToken are both set
 	p.nextToken()
 	p.nextToken()
 
 	return p
+}
+
+func (p *Parser) Errors() []string {
+	return p.errors
+}
+
+func (p *Parser) peekError(t token.TokenType) {
+	msg := fmt.Sprintf("expected next token to be %s, got %s insted",
+		t, p.peekToken.Type)
+	p.errors = append(p.errors, msg)
 }
 
 func (p *Parser) nextToken() {

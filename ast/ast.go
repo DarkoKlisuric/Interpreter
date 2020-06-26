428fa9e242c863a1fccb36164fe0137bc25ccb9d
Darko Klisurić <klisuric1995@gmail.com>
2020-06-26 23:01:42 +0200

    String method for letStatement

--- ast/ast.go
+++ ast/ast.go
@@ -77,3 +77,20 @@ func (p *Program) String() string {
 
 	return out.String()
 }
+
+func (ls *LetStatement) String() string {
+	var out bytes.Buffer
+
+	out.WriteString(ls.TokenLiteral() + " ")
+	out.WriteString(ls.Name.String())
+	out.WriteString(" = ")
+
+	if ls.Value != nil {
+		out.WriteString(ls.Value.String())
+	}
+
+	out.WriteString(";")
+
+	return out.String()
+}
+

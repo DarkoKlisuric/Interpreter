e003644b3cf29bc606bc7864cace3fe357456382
Darko Klisurić <klisuric1995@gmail.com>
2020-06-30 20:15:17 +0200

    Creating InfixEpxression and declaring method for this type

--- ast/ast.go
+++ ast/ast.go
@@ -54,9 +54,16 @@ type IntegerLiteral struct {
 }
 
 type PrefixExpression struct {
-	Token token.Token // The prefix token, e.g. !
+	Token    token.Token // The prefix token, e.g. !
 	Operator string
-	Right Expression
+	Right    Expression
+}
+
+type InfixExpression struct {
+	Token    token.Token // The operator token, e.g. +
+	Left     Expression
+	Operator string
+	Right    Expression
 }
 
 func (i *Identifier) expressionNode()      {}
@@ -76,7 +83,7 @@ func (il *IntegerLiteral) expressionNode()      {}
 func (il *IntegerLiteral) TokenLiteral() string { return il.Token.Literal }
 func (il *IntegerLiteral) String() string       { return il.Token.Literal }
 
-func (pe *PrefixExpression) expressionNode()  {}
+func (pe *PrefixExpression) expressionNode()      {}
 func (pe *PrefixExpression) TokenLiteral() string { return pe.Token.Literal }
 func (pe *PrefixExpression) String() string {
 	var out bytes.Buffer
@@ -84,6 +91,20 @@ func (pe *PrefixExpression) String() string {
 	out.WriteString("(")
 	out.WriteString(pe.Operator)
 	out.WriteString(pe.Right.String())
+	out.WriteString(")")
+
+	return out.String()
+}
+
+func (oe InfixExpression) expressionNode()       {}
+func (oe *InfixExpression) TokenLiteral() string { return oe.Token.Literal }
+func (oe *InfixExpression) String() string {
+	var out bytes.Buffer
+
+	out.WriteString("(")
+	out.WriteString(oe.Left.String())
+	out.WriteString(" " + oe.Operator + " ")
+	out.WriteString(oe.Right.String())
 	out.WriteString(")")
 
 	return out.String()

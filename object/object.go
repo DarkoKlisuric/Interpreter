401df272cc8fb843e846a4d568a0557a9eeadc06
Darko Klisurić <klisuric1995@gmail.com>
2020-07-09 19:54:10 +0200

    Null object

--- object/object.go
+++ object/object.go
@@ -7,6 +7,7 @@ type ObjectType string
 const (
 	IntegerObj = "INTEGER"
 	BooleanObj = "BOOLEAN"
+	NullObj    = "NULL"
 )
 
 // represent every value which is encounter when evaluating source code as an Object
@@ -23,8 +24,13 @@ type Boolean struct {
 	Value bool
 }
 
-func (i *Integer) Type() ObjectType { return IntegerObj }
-func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+type Null struct{}
 
-func (i *Boolean) Type() ObjectType { return BooleanObj }
-func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value)}
+func (i *Integer) Type() ObjectType { return IntegerObj }
+func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
+
+func (b *Boolean) Type() ObjectType { return BooleanObj }
+func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
+
+func (n *Null) Type() ObjectType { return NullObj }
+func (n *Null) Inspect() string  { return "null" }

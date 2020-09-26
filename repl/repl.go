8cab89b853bb8997e0d44b7b505a191788b6e8a2
Darko Klisurić <klisuric1995@gmail.com>
2020-09-26 10:17:04 +0200

    Ignoring explicitly

--- repl/repl.go
+++ repl/repl.go
@@ -39,14 +39,14 @@ func Start(in io.Reader, out io.Writer) {
 		evaluated := evaluator.Eval(program, env)
 
 		if evaluated != nil {
-			io.WriteString(out, evaluated.Inspect())
-			io.WriteString(out, "\n")
+			_, _ = io.WriteString(out, evaluated.Inspect())
+			_, _ = io.WriteString(out, "\n")
 		}
 	}
 }
 
 func printParserErrors(out io.Writer, errors []string) {
 	for _, msg := range errors {
-		io.WriteString(out, "\t"+msg+"\n")
+		_, _ = io.WriteString(out, "\t"+msg+"\n")
 	}
 }
